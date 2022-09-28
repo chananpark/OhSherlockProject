e65f97d8176969e8351a4ad364815f8b445cd83a
chaeo <riverchae0@gmail.com>
2022-09-28 16:02:13 +0900

    삭제파일 수정파일 커밋

--- OhSherlockProject/src/main/webapp/WEB-INF/sql/chae0.sql
+++ OhSherlockProject/src/main/webapp/WEB-INF/sql/chae0.sql
@@ -3,7 +3,8 @@ show user;
 
 
 select *
-from tbl_member;
+from tbl_member
+order by registerday desc;
 
 rollback;
 
@@ -12,4 +13,41 @@ from tbl_coin_history;
 
 select *
 from tbl_login_history;
+
+create sequence seq_coin_history
+start with 1
+increment by 1
+nomaxvalue
+nominvalue
+nocycle
+nocache;
+
+
+
+insert into tbl_coin_history(COINNO, FK_USERID, COIN_DATE, COIN_AMOUNT) values(1,'codud1158', sysdate - 3, 2000);
+
+insert into tbl_coin_history(COINNO, FK_USERID, COIN_DATE, COIN_AMOUNT)
+ values(seq_coin_history.nextval, 'codud1158',sysdate - 3, 2000);
+
+ commit;
+ 
+ 
+select COINNO, FK_USERID,  COIN_DATE, COIN_AMOUNT
+from tbl_coin_history;
+
+
+
+SELECT userid, coin, COINNO,  COIN_DATE, COIN_AMOUNT
+FROM 
+(
+    select userid, coin
+    from tbl_member
+    where status = 1 and userid = 'codud1158' and PASSWD = '9695b88a59a1610320897fa84cb7e144cc51f2984520efb77111d94b402a8382' 
+) M 
+CROSS JOIN 
+(
+    select COINNO, COIN_DATE, COIN_AMOUNT
+    from tbl_coin_history
+    where fk_userid = 'codud1158'
+) H;
 
